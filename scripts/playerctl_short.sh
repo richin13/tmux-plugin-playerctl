367e70aee487e7ff5a70fe06780852f64b2a42f2
Ricardo Madriz <rmadriz@keplergrp.com>
2021-11-26 13:48:38 -0600

    Simplify short script

--- scripts/playerctl_short.sh
+++ scripts/playerctl_short.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 main() {
-  local MAX_CHARS=40
-  local full=$(playerctl metadata --format "{{ artist }} - {{ title }}")
-  local shorten=$(echo $full | cut -c 1-$MAX_CHARS)
-  local info_char_count=$(echo $full | wc -c)
+  local full shortened info_char_count
+  local MAX_CHARS=${PLAYERCTL_MAX_CHARS:-40}
+  full=$(playerctl metadata --format "{{ artist }} - {{ title }}")
+  shortened=${full:0:$MAX_CHARS}
+  info_char_count=${#full}
   local append=""
 
   if [[ $info_char_count -gt $MAX_CHARS ]]; then
     append="..."
   fi
 
-  echo "$shorten$append"
+  echo "$shortened$append"
 }
 
 main
